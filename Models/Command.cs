b7ddfa2576e648b3c4adf4e2b196b0f65d7a0ccf
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-07-03 18:48:53 -0300

    Configuração do banco de dados

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models
 {
     public class Command
     {
+        // convenção sobre configuração, é Id e tem todas as características de um ID
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
